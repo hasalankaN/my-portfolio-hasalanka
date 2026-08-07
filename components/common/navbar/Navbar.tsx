fed71414d3d03ecfe1644f69e9449e34422fdc3b
Hasalanka.N <hasalanka.nipun@wisesoftlab.com>
2026-08-08 00:14:51 +0530

    fix

--- components/common/navbar/Navbar.tsx
+++ components/common/navbar/Navbar.tsx
@@ -33,7 +33,7 @@ export function Navbar() {
           : "bg-transparent"
       }`}
     >
-      <div className="max-w-6xl mx-auto px-8 sm:px-1 py-4 flex items-center justify-between">
+      <div className="max-w-full mx-auto px-15 sm:px-10 py-4 flex items-center justify-between">
         {/* Logo */}
         <Link
           href="/"
