e8b93673a4550196cb250741c8b59875cc8707ca
Hasalanka.N <hasalanka.nipun@wisesoftlab.com>
2026-08-08 14:48:05 +0530

    fix

---
+++ components/common/navbar/LecturerNavbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import type { SidebarNavItem } from "@/data/sidebar";
+
+interface LecturerNavbarProps {
+  filteredSidebarData: SidebarNavItem[];
+}
+
+export default function LecturerNavbar({ filteredSidebarData: _ }: LecturerNavbarProps) {
+  return null;
+}
